484d6b4664e3fed4f613c4efd778422fa012e0ef
F-smile-hahaha <1579113019@qq.com>
2019-12-08 23:54:44 +0800

    写代码可以在整型有序数组中查找想要的数字， 找到了返回下标，找不到返回 - 1.（折半查找）

---
+++ 015.c
@@ -0,0 +1,35 @@
+//写代码可以在整型有序数组中查找想要的数字，
+//找到了返回下标，找不到返回 - 1.（折半查找）
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#define n 100
+int main()
+{
+	int num[n];
+	int left = 0;
+	int right = sizeof(num) / sizeof(num[0]) - 1;
+	int mid = 0;
+	int key;
+	for (int i = 0; i <= n; i ++ ){
+		num[i] = i+1;
+	}
+	printf("输入你想查找的数字:");
+	scanf("%d", &key);
+	while (left <= right){
+			mid = (left + right) / 2;
+			if (key > num[mid]){
+				left = mid++;
+			}
+			else if (key < num[mid]){
+				right = mid--;
+			}
+			else{
+				printf("找到了,下标为： %d \n", mid);
+				break;
+			}
+	}
+	putchar('-1');
+	system("pause");
+	return 0;
+}
